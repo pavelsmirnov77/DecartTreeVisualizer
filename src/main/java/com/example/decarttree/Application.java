eddffdf6c5635c6ccc1de6a586bef8e0cf0e6774
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-05-05 00:15:10 +0300

    Добавлена иконка приложения

--- src/main/java/com/example/decarttree/Application.java
+++ src/main/java/com/example/decarttree/Application.java
@@ -2,6 +2,7 @@ package com.example.decarttree;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,6 +10,8 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Decart tree visualizer");
+        stage.getIcons().add(new Image("D:\\DecartTree\\src\\main\\resources\\com\\example\\decarttree\\image_tree.png"));
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("decart-tree-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1495, 800);
         stage.setScene(scene);
